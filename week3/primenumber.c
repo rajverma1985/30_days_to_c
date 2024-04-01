daedd3917e9f877cd10b05fd564115fe8a6bc57d
Rajneesh Verma <rjnshvrm23@gmail.com>
2024-04-01 11:36:23 +0200

    prime numbers

--- week3/primenumber.c
+++ week3/primenumber.c
@@ -11,9 +11,24 @@ rules:
 */
 
 
-int main(){
-	// pre condition for prime numbers: 0, 1 are not prime 2 is the smallest even prime  number.
-	
-}
+int main() {
+    // pre condition for prime numbers: 0, 1 are not prime 2 is the smallest even prime number.
+    int primeNumber[100] = {2, 3};
+    int n, i;
 
+    for (n = 5; n <= 100; n = n + 2) {
+        int isPrime = 1; // Assume n is prime initially
+        for (i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                isPrime = 0; // If n is divisible by any number other than 1 and itself, it's not prime
+                break;
+            }
+        }
+        if (isPrime) {
+            printf("the prime number is %d\n", n);
+        }
+    }
+
+    return 0;
+}
 
